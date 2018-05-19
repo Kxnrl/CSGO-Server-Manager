4a111a2504e9de75f3dd6928b843ad0b36c8793b
Kyle <kyle@kxnrl.com>
2018-05-19 12:11:53 +0800

    update Assembly

--- CSGO-Server-Manager/Properties/AssemblyInfo.cs
+++ CSGO-Server-Manager/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("CSGO-Server-Manager")]
+[assembly: AssemblyTitle("Simple CSGO Dedicated Server Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CSGO-Server-Manager")]
 [assembly: AssemblyCopyright("Copyright ©2018 Kyle")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("No")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
